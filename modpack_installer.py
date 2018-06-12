3d10cd367d7d0b25f741c5739070ea2cd987ace1
kiln707 <steven.guy.swanson@gmail.com>
2018-06-12 11:28:59 -0700

    Created Test files. Successful installation of modpack

--- modpack_installer.py
+++ modpack_installer.py
@@ -1,14 +1,17 @@
+from io import BytesIO
+from itertools import product
 from tkinter import *
-from multiprocessing import Process
+from zipfile import ZipFile
+from multiprocessing import Process, Pool
 import json
-import urllib.request
+import urllib.request as request
 import os
 
 def download(url=None):
     if not url:
         return None
     try:
-        response = urllib.request.urlopen(url)
+        response = request.urlopen(url)
         return response.read()
     except:
         return None
@@ -19,7 +22,7 @@ def filename_from_url(url=None):
         return str(url)[int(pos):]
 
 def download_json(url=None):
-    data = download_file(url)
+    data = download(url)
     if data:
         return json.loads(data.decode('utf-8'))
     return None
@@ -50,36 +53,44 @@ def get_latest_json(url=None, file_location=None):
         return None
 
 def make_modpack_directories(modpack, data_directory=''):
+    print(modpack_isInstalled(modpack, data_directory))
     if not modpack_isInstalled(modpack, data_directory):
         os.mkdir(os.path.join(data_directory, modpack[0]))
     if not os.path.isdir(os.path.join(data_directory, modpack[0], "mods")):
         os.mkdir(os.path.join(data_directory, modpack[0], "mods"))
     if not os.path.isdir(os.path.join(data_directory, modpack[0], 'config')):
         os.mkdir(os.path.join(data_directory, modpack[0], "config"))
-    return os.mkdir(os.path.join(data_directory, modpack[0]))
+    return os.path.join(data_directory, modpack[0])
 
 def install_config_files(config_dir, url=''):
-    pass
+    resp = download(url)
+    zipfile = ZipFile(BytesIO(resp))
+    zipfile.extractall(config_dir)
 
-def install_mod_files(mod_dir, (modname, modversion, modinfo, modurl)):
-    pass
-
+def install_mod_files(mod_data):
+    data = download(mod_data[3])
+    with open(os.path.join(mod_data[4], "%s-%s.jar"%(mod_data[0],mod_data[1])), "wb+") as f:
+        f.write(data)
+    print("Downloaded %s, Version: %s"%(mod_data[0], mod_data[1]))
 
 def install_modpack(modpack, data_directory=''):
     modpack_dir=make_modpack_directories(modpack, data_directory)
     mod_dir=os.path.join(modpack_dir, "mods")
     config_dir=os.path.join(modpack_dir, "config")
     modpack_json=get_latest_json(url=modpack[1],file_location=os.path.join(modpack_dir, filename_from_url(modpack[1])))
-    config_dl=Process(target=install_config_files, kwargs={'config_dir':config_dir, 'url':modpack_json['config_link']})
-    config_dl.start()
+    config_dl=None
+    if modpack_json['config_link']:
+        config_dl=Process(target=install_config_files, kwargs={'config_dir':config_dir, 'url':modpack_json['config_link']})
+        config_dl.start()
+#    modlist = modpack_json['modlist']
+    for modinfo in modpack_json['modlist']:
+        modinfo.append(mod_dir)
     with Pool() as pool:
-        pool.map(install_mod_files, modpack_json['modlist'])
-
-
-
-
-
-
+        pool.imap_unordered(install_mod_files, modpack_json['modlist'])
+        pool.close()
+        pool.join()
+    if config_dl:
+        config_dl.join()
 
 def modpack_isInstalled(modpack, data_directory=''):
     return os.path.isdir(os.path.join(data_directory, modpack[0]))
@@ -99,9 +110,8 @@ def update_available(modpack, data_directory=''):
 
 if __name__ == "__main__":
     #Editable Variables for installer
-    MANIFEST_URL = ""
-    DATA_DIR_NAME=""
-    SERVERNAME=""
+    MANIFEST_URL = "http://relatedbygaming.ddns.net/files/rbgtest.manifest"
+    DATA_DIR_NAME=".RelatedByGaming"
 
     #No more configuration
     data_directory=''
@@ -110,17 +120,18 @@ if __name__ == "__main__":
     else:
         data_directory=os.path.join(os.path.expanduser(), DATA_DIR_NAME)
 
-    manifest_filename=os.path.join(data_directory, filename_from_url(MANIFEST_URL))
-    manifest = get_latest_manifest(url=MANIFEST_URL, file_location=manifest_filename)
+    manifest_filename=os.path.join(data_directory, str(filename_from_url(MANIFEST_URL)))
+    manifest = get_latest_json(url=MANIFEST_URL, file_location=manifest_filename)
     if not manifest:
         exit(1)
+    print(manifest)
     modpack_list=manifest['modlist']
 
     for modpack in modpack_list:
-        if not modpack_isInstalled(modpack, DATA_DIR_NAME):
-            install_modpack(modpack, DATA_DIR_NAME)
-        elif modpack_update_available(modpack):
-            update_modpack(modpack, DATA_DIR_NAME)
+        if not modpack_isInstalled(modpack, data_directory):
+            install_modpack(modpack, data_directory)
+        #elif modpack_update_available(modpack):
+        #    update_modpack(modpack, DATA_DIR_NAME)
 
 
     #modpack_list_status={}
